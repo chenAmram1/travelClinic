59118d5169879c01e84ba4cd936c271e9c624a5b
chenAmram1 <henramram@gmail.com>
2024-02-27 21:28:03 +0200

    testing the ap.js

--- code/backend/app.js
+++ code/backend/app.js
@@ -1,8 +1,25 @@
+
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-//=========================
+
+  //=========================
+
+
+  app.get("/test", (req, res) => {
+    res.json({
+      fname:"chen"
+    
+
+    });
+      
+    
+  })
+
+
+  
+app.use(express.static('frontend'));
+
 app.listen(3001, function () {
-console.log('My app is listening on port 3001!');
+    console.log('My app is listening on port 3001!');
 });
---
+++ code/backend/frontend/index.js
@@ -0,0 +1,30 @@
+function myfunction()
+{
+    fetch ("https://randomuser.me/api/?results=50")
+    .then((dataAsStringFromOurApi)=>{
+       return dataAsStringFromOurApi.json(); 
+    })
+     .then ((data)=>{
+         document.querySelector ("#mydiv").innerHTML=
+         data.results[0].name.first;
+
+     })
+}
+
+myfunction();
+
+
+function getMyownData()
+{
+    fetch ("http://localhost:3001/test")
+    .then((dataAsStringFromOurApi)=>{
+       return dataAsStringFromOurApi.json(); 
+    })
+     .then ((data)=>{
+         document.querySelector ("#mydiv1").innerHTML=
+         data.fname;
+
+     })
+}
+
+getMyownData();
